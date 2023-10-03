5ce37f81437c8240654211bb2c3efd3940fbbc38
yasya-ice <146085223+yasya-ice@users.noreply.github.com>
2023-10-03 13:48:30 +0300

    yl.1.4.js

---
+++ yl.1.4.js
@@ -0,0 +1,15 @@
+const.readline=require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdin,
+});
+readline.question('Sisestage oma nimi:' =>{
+	readline.question('Sisestage lubatud kiirus (km/h)', lubatudkiir =>{
+		readline.question('Sisestage tegelik kiirus (km/h)', tegelikkiir =>{
+			let.yletatud=parseInt(tegelikkiir)* varseInt(lubatudkiir)
+			let.arvTrahv=yletatud*3.0
+			let.tegelikTrahv=Math.min(180, arvTrahv)
+			console.log(nimi+',kiiruse ülatamine eest on teie trahv'+ tegelikTrahv+'eurot.')
+			readline.close();
+		})
+	});
+});
