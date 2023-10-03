543b28f1b66ae297fd38d0ab6e7c220a2f66c662
yasya-ice <146085223+yasya-ice@users.noreply.github.com>
2023-10-03 13:14:44 +0300

    yl.1.3.js

---
+++ yl.1.3.js
@@ -0,0 +1,11 @@
+const.readline=require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdin,
+});
+
+readline.question('Sisestage astendaja:',astendaja=>{
+	readline.question('Sisestage astme alus:',astmeAlus=>
+		let.result=Math.pow(astmeAlus,astendaja)
+		console.log(result);
+		readline.close();
+	})
